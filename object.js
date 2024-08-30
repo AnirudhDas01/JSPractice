76103a1dbeb04efc96df31aa1ec3d65ba598f7cb
Anirudh Das <dasanirudh5@gmail.com>
2024-08-30 12:22:44 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,19 @@
+var arrayObj = [
+    {
+        "authorName" : "Anirudh",
+        "book" : ["ABC" , "DEF" ],
+        "rating" : 4.5
+    },
+    {
+        "authorName" : "Ashmit",
+        "book" : ["GHI" , "JKL"],
+        "rating" : 4.8
+    },
+    {
+        "authorName" : "Arpit",
+        "book" : ["MNO" , "PQR"],
+        "rating" : 4.1
+    }
+]
+
+console.log(arrayObj[2]["book"][0]);
---
+++ string.js
@@ -0,0 +1,2 @@
+
+console.log('anirudh' * 4);
